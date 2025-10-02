a4996790acb229513860a37c71506fa35187bbaa
devc-a <gutoafkz@hotmail.com>
2025-10-02 16:05:42 -0300

    created bd.ts
    created the logic of connection to database

---
+++ bd.ts
@@ -0,0 +1,8 @@
+   import  {Pool}  from "pg";
+   export const pool = new Pool({
+       user: 'postgres',
+       host: 'localhost',
+         database: 'project',
+            password: 'admin',
+            port: 5432,
+   });
